9a9d320f7eee2ddf146ca16adbbcbfd990339774
lucasFrt <lucasmigfrota03@gmail.com>
2025-07-29 18:39:24 -0300

    melhorias de css

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,14 @@
-import React, { useEffect, useState } from 'react';
 import './App.css'
-import type { AgenteType } from './types/agente.type';
 import { HeaderComponents } from './components/header/HeaderComponent';
+import { AgentsComponent } from './components/agents/agentsComponent';
 
-//app
-function App() {
-  //hooks
-  //useState para armazenar os agentes 
-  const [agentes, setAgentes] = useState<Array<AgenteType>>([]);
-  const [todosAgentes, setTodosAgentes] = useState<Array<AgenteType>>([]);
-  const [pesquisa, setPesquisa] = useState("");
-  //URL da API
-  const URLagents = "https://valorant-api.com/v1/agents";
 
-  //useEffct busca API (agentes)
-  useEffect(() => {
-    
-    fetch(URLagents)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Requisição falhou: " + response.statusText);
-        }
-        return response.json();
-      })
-      .then(response => {
-        // setAgentes(response.data);
-        setTodosAgentes(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
-  useEffect(() => {
-    
-    const agentesFiltrados = todosAgentes.filter((agente) =>
-      
-      agente.displayName.toLowerCase().includes(pesquisa.toLowerCase())
-      );
-      setAgentes(agentesFiltrados)
-  },[pesquisa, todosAgentes])
-  
-      
-  //retorno (pagina principal)
-  return (
-
-    <div className={'agentes-container'}>
+export default function App() {
+  return(
+    <div className='main'> 
       <HeaderComponents />
-      <br />
-      <h1>Agentes</h1>
-      <label className="pesquisa" >
-        Pesquisar agentes: <input name="Pesquisa" className="pesquisando" onChange={(valor) => { 
-          setPesquisa(valor.target.value);
-        }} /> 
-      </label>
-      <hr />
-      <br />
-      <div className="agentes-grid">
-        
-
-        {agentes.map((agente) => (
-        <div className='listaAgentes' key={agente.uuid}>
-          <div className="agente-nome">
-            <p>{agente.displayName}</p>
-          </div>
-          <img src={agente.displayIcon} alt={agente.displayName} />
-        </div>
-        ))}
-        </div>
+      <AgentsComponent />
     </div>
   );
- }
-export default App
+};
+
---
+++ src/components/agents/AgentsComponent.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import './agents-component.css'
+import type { AgenteType } from '../../types/agente.type';
+
+
+//app
+export function AgentsComponent() {
+  //hooks
+  //useState para armazenar os agentes 
+  const [agentes, setAgentes] = useState<Array<AgenteType>>([]);
+  const [todosAgentes, setTodosAgentes] = useState<Array<AgenteType>>([]);
+  const [pesquisa, setPesquisa] = useState("");
+  //URL da API
+  const URLagents = "https://valorant-api.com/v1/agents";
+
+  //useEffct busca API (agentes)
+  useEffect(() => {
+    
+    fetch(URLagents)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Requisição falhou: " + response.statusText);
+        }
+        return response.json();
+      })
+      .then(response => {
+        // setAgentes(response.data);
+        setTodosAgentes(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+  useEffect(() => {
+    
+    const agentesFiltrados = todosAgentes.filter((agente) =>
+      
+      agente.displayName.toLowerCase().includes(pesquisa.toLowerCase())
+      );
+      setAgentes(agentesFiltrados)
+  },[pesquisa, todosAgentes])
+  
+      
+  //retorno (pagina principal)
+  return (
+
+    <div className={'agentes-container'}>
+      <br />
+      <h1>Agentes</h1>
+      <label className="texto-pesquisa" >
+        Pesquisar agentes: <input name="Pesquisa" className="caixa-pesquisa" onChange={(valor) => { 
+          setPesquisa(valor.target.value);
+        }} /> 
+      </label>
+      <hr />
+      <br />
+      <div className="agentes-grid">
+        
+
+        {agentes.map((agente) => (
+        <div className='listaAgentes' key={agente.uuid}>
+          <div className="agente-nome">
+            <p>{agente.displayName}</p>
+          </div>
+          <img src={agente.displayIcon} alt={agente.displayName} />
+        </div>
+        ))}
+        </div>
+    </div>
+  );
+ }
